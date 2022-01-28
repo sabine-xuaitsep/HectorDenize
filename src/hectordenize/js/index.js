b7f3d3a5d3584a5f73a56e41fe24d899354e63ff
sabine-xuaitsep <sabinepestiaux@gmail.com>
2022-01-28 20:26:56 +0100

    Manual page for framework added

--- src/hectordenize/js/index.js
+++ src/hectordenize/js/index.js
@@ -5,3 +5,10 @@ import {navTrigger} from './components/_nav_trigger'; navTrigger();
 
 import {navScrollSpy} from './components/_scrollspy'; navScrollSpy();
 import {headerScrollSpy} from './components/_scrollspy'; headerScrollSpy();
+
+
+// Basic email protection
+const mailPart1 = "webdeveloper.ieps";
+const mailPart2 = "@";
+const mailPart3 = "gmail.com";
+document.getElementById("mailAddress").innerHTML = `<a href=mailto:${mailPart1 + mailPart2 + mailPart3} class="color-dim">${mailPart1 + mailPart2 + mailPart3}</a>.`;
